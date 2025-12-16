4635c8bf258f26be3bcc8be16fdde3df12bc773e
detroitlionssilva-wq <detroit.lions.silva@gmail.com>
2025-12-16 14:01:47 -0300

    Add initial HTML structure for card slider

---
+++ main.js
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet">
+    <a href="https://cdnjs.cloudflare.com/ajax/libs/swiper/6.4.5/swiper-bundle.min.css"></a>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="style.css">
+ 
+    <title>card slider</title>
+</head>
+<body>
+
+<main class="slider-container">
+
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+
+            <!-- Slider 1 -->
+            <div class="slider-item swiper-slide">
+                <div class="slider-image-wrapper">
+                    <img src="1.jpg" alt="slider 1 image" class="slider-image">
+                </div>
+                <div class="slider-item-content">
+                    <h2>Slide 1</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dicta nihil velit ab quisquam omnis quidem.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Slider 2 -->
+            <div class="slider-item swiper-slide">
+                <div class="slider-image-wrapper">
+                    <img src="1.jpg" alt="slider 1 image" class="slider-image">
+                </div>
+                <div class="slider-item-content">
+                    <h2>Slide 2</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dicta nihil velit ab quisquam omnis quidem.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Slider 3 -->
+             <div class="slider-item swiper-slide">
+                <div class="slider-image-wrapper">
+                    <img src="1.jpg" alt="slider 1 image" class="slider-image">
+                </div>
+                <div class="slider-item-content">
+                    <h2>Slide 3</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dicta nihil velit ab quisquam omnis quidem.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Slider 4 -->
+            <div class="slider-item swiper-slide">
+                <div class="slider-image-wrapper">
+                    <img src="1.jpg" alt="slider 1 image" class="slider-image">
+                </div>
+                <div class="slider-item-content">
+                    <h2>Slide 4</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dicta nihil velit ab quisquam omnis quidem.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- Pagination -->
+        <div class="swiper-pagination"></div>
+    </div>
+</main>
+
+<!-- Scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/swiper/6.4.5/swiper-bundle.min.js"></script>
+<script src="script.js"></script>
+
+</body>
+</html>
